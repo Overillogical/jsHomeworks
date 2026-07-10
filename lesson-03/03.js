8bb0744ad981346eaae8585216764350e121e927
AlexanderS <ksellos99@gmail.com>
2026-07-10 05:10:25 +0300

    Done howerks for lesson03

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,19 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(number1,number2,number3) {
+    let largestNumber = number1;
+    if (largestNumber <number2){
+        largestNumber=number2
+        if (largestNumber<number3){
+            return number3
+        }else{
+            return number2
+        }
+    }else{
+        if(largestNumber<number3){
+            return number3
+        }else{
+            return number1
+        }
+    }
+}
